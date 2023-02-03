36901d537143aa60560cc54f26f45a27d69b69af
Mousa Sweidan <msweidan@hct.ac.ae>
2023-02-03 10:35:55 +0400

    Initial Commit

---
+++ Day4/Day4App.swift
@@ -0,0 +1,17 @@
+//
+//  Day4App.swift
+//  Day4
+//
+//  Created by msweidan on 03/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Day4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
